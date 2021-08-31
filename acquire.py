3b0144eb1be7f5ff987592ca8603eead7f165e97
JacobPaxton <jacob.paxton65@gmail.com>
2021-08-31 09:01:52 -0500

    add completed acquire script

--- acquire.py
+++ acquire.py
@@ -3,24 +3,36 @@ import os
 from env import get_db_url
 
 def gen_telco():
+    """
+        Checks for local copies of telco_churn tables,
+        If tables aren't stored locally, 
+            Reads the telco_churn database from Codeup, and
+            Saves each table to the current directory as separate .csv files, then
+        Returns each table separately.
+    """
+
+    # customers table
     if not os.path.isfile('telco_raw.csv'):
         url = get_db_url(db_name='telco_churn')
         telco_raw = pd.read_sql('SELECT * FROM customers', url)
         telco_raw.to_csv('telco_raw.csv')
     telco_raw = pd.read_csv('telco_raw.csv')
 
+    # contract_types table
     if not os.path.isfile('contract_types.csv'):
         url = get_db_url(db_name='telco_churn')
         telco_raw = pd.read_sql('SELECT * FROM contract_types', url)
         telco_raw.to_csv('contract_types.csv')
     contract_types = pd.read_csv('contract_types.csv')
 
+    # internet_service_types table
     if not os.path.isfile('internet_service_types.csv'):
         url = get_db_url(db_name='telco_churn')
         telco_raw = pd.read_sql('SELECT * FROM internet_service_types', url)
         telco_raw.to_csv('internet_service_types.csv')
     internet_service_types = pd.read_csv('internet_service_types.csv')
 
+    # payment_types table
     if not os.path.isfile('payment_types.csv'):
         url = get_db_url(db_name='telco_churn')
         telco_raw = pd.read_sql('SELECT * FROM payment_types', url)
